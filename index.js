283d513503b63d506f400931073b3a3cbea326e0
shrivashadi <shrivashad@gmail.com>
2026-06-21 16:16:39 +0530

    Initial commit with app and dockerfile

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World! Staging Deployment Successful!</h1>');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
